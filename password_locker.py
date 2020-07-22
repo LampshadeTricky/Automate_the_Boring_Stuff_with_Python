35db442836423875a2fa85d854fdc74df039324e
jbrown <jonathanbrown.42@gmail.com>
2020-07-21 20:23:25 -0400

    Testing

--- password_locker.py
+++ password_locker.py
@@ -1,3 +1,5 @@
 !# python 3
 
-# This is a password locker example which lets the user create complicated passwords and save them ithout having to remember everything
+# This is a password locker example which lets the user create complicated 
+# passwords and save them ithout having to remember everything.
+
